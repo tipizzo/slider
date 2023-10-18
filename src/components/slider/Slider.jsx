0eed24fff820971a59232d98a33104f14b26cf58
KABANGA David <Davkabanga96@gmail.com>
2023-10-18 16:08:08 +0200

    Adding logical part

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
 import "./slider.css";
+import sliderData from "../../data/sliderData";
 
 function Slider() {
+    const [sliderIndex, setSliderIndex] = useState(1);
+
+    function toggleImage(indexPayload) {
+        let newState
+        if(indexPayload + sliderIndex > sliderData.length) {
+            newState = 1
+        }
+
+        else if(indexPayload + sliderIndex < 1) {
+            newState = sliderData.length
+        }
+
+        else {
+            newState = indexPayload + sliderIndex
+        }
+
+        setSliderIndex(newState);
+    }
+
     return(
         <>
-            <p className="index-info">3 / 5</p>
+            <p className="index-info">{sliderIndex} / {sliderData.length}</p>
             <div className="slider">
-                <p className="image-info">Bedroom</p>
-                <img src="/images/img-3.jpg" alt="estate rooms" className="slider-img" />
+                <p className="image-info">{sliderData.find(obj => obj.id === sliderIndex).description}</p>
+                <img src={`/images/img-${sliderIndex}.jpg`} alt="estate rooms" className="slider-img" />
 
-                <button className="navigation-button prev-button">
+                <button
+                onClick={() => toggleImage(-1)}
+                 className="navigation-button prev-button">
                     <img src={leftChevron} alt="previous image" />
                 </button>
-                <button className="navigation-button next-button">
+                <button
+                onClick={() => toggleImage(1)}
+                 className="navigation-button next-button">
                     <img src={rightChevron} alt="next image" />
                 </button>
             </div>
